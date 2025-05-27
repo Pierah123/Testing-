e317257bfed9d92b682ac3fecfea43f22c18b54e
Pierah123 <25645@student.macleans.school.nz>
2025-05-27 19:50:39 +1200

    Create Even Numbers.py

---
+++ Even Numbers.py
@@ -0,0 +1,9 @@
+number = int(input("Enter number: "))
+
+def getEven(n):
+    if n % 2 == 0:
+        print("Your number is even")
+    else:
+        print("Your number is not an even number")
+
+getEven(number)
